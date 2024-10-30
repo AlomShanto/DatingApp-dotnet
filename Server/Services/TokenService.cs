2c2e47f27cb70c62bb6e1f58f892ba5ffe15e709
Md. Shuhayel Alom Shanto <shanto@orbitaxbd.com>
2024-10-30 12:56:47 +0600

    jwt token generation

--- Server/Services/TokenService.cs
+++ Server/Services/TokenService.cs
@@ -1,13 +1,37 @@
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using Server.Interfaces;
 using Server.Models;
 
 namespace Server.Services;
 
-public class TokenService : ITokenService
+public class TokenService(IConfiguration config) : ITokenService
 {
     public string CreateToken(UserModel user)
     {
-        throw new NotImplementedException();
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from appsettings");
+        if(tokenKey.Length<64) throw new Exception("Your token needs to be longer");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>{
+            new(ClaimTypes.NameIdentifier, user.UserName)
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokemDescriptor = new SecurityTokenDescriptor{
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokemDescriptor);
+
+        return tokenHandler.WriteToken(token);
     }
 }
